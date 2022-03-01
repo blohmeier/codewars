3e91518d2aa82e0fe71aee314b31dd485e804f29
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-01 08:28:08 -0600

    adds description and starts code for 'findTheParityOutlier_3.js'

--- 2_squareEveryDigit.js
+++ 2_squareEveryDigit.js
@@ -4,3 +4,13 @@ Welcome. In this kata, you are asked to square every digit of a number and conca
 For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 Note: The function accepts an integer and returns an integer
  */
+function squareDigits(num){
+    let array = num.toString().split("");
+    let separateArray = array.map(x => parseInt(x));
+    let squaredArray = separateArray.map(function (x) {
+        return Math.pow(x,2);
+    })
+    let joinedArray = squaredArray.join("");
+    return parseInt(joinedArray);
+}
+console.log(squareDigits(9119));
---
+++ findTheParityOutlier_3.js
@@ -0,0 +1,15 @@
+//3. Find the Parity Outlier
+/*
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+Example 1:
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+Example 2:
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+ */
+function findOutlier(integers){
+    for (let i = 0; i = integers.length - 1; i++) {
+
+    }
+}
