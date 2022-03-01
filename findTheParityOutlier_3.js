29b0ee5214083949715e83fb5757409ec1407d29
Brett Lohmeier <brett.lohmeier@gmail.com>
2022-03-01 08:35:07 -0600

    updates code for 'findTheParityOutlier_3.js'

--- findTheParityOutlier_3.js
+++ findTheParityOutlier_3.js
@@ -9,7 +9,12 @@ Example 2:
 Should return: 160 (the only even number)
  */
 function findOutlier(integers){
-    for (let i = 0; i = integers.length - 1; i++) {
-
-    }
+    let even = integers.filter(a => a % 2 === 0);
+    let odd = integers.filter(a  => a % 2 !== 0);
+    return even.length === 1 ? even[0] : odd[0];
 }
+console.log(findOutlier([0, 1, 2]));
+console.log(findOutlier([1, 2, 3]));
+console.log(findOutlier([2,6,8,10,3]));
+console.log(findOutlier([0,0,3,0,0]));
+console.log(findOutlier([1,1,0,1,1]));
